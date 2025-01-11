a9a0253a6a146189d8f79b3ff693cafc6acca42f
systemnotmysister <48369226+systemnotmysister@users.noreply.github.com>
2025-01-11 18:52:10 +0200

    upgraded svelte cfg

--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,20 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    // Используем адаптер для Cloudflare
-    adapter: cloudflare(),
-  }
+    // Используем адаптер для Cloudflare с дополнительными настройками
+    adapter: cloudflare({
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>'],
+      },
+      platformProxy: {
+        configPath: 'wrangler.toml', // путь к конфигу wrangler.toml
+        environment: undefined, // можно указать окружение
+        experimentalJsonConfig: false,
+        persist: false,
+      }
+    }),
+  },
 };
 
 export default config;
